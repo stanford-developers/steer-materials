f6b2115f608b6648da421b2bb69e609d81a39f3f
Nicholas Siemons <nicholas9182@gmail.com>
2025-08-07 18:13:20 -0700

    moved_classes_from_CDT

---
+++ steer_materials/Base.py
@@ -0,0 +1,145 @@
+from steer_core.Constants.Units import *
+from steer_core.Mixins.Validators import ValidationMixin
+from steer_core.Mixins.Serializer import SerializerMixin
+
+from datetime import datetime as dt
+
+import numpy as np
+
+
+class _Material(ValidationMixin, SerializerMixin):
+
+    def __init__(
+            self, 
+            name: str,
+            density: float, 
+            specific_cost: float,
+            color: str
+        ):
+        """
+        Metal object for encapsulation of the cell
+        
+        Parameters
+        ----------
+        name : str
+            Name of the material.
+        density : float
+            Density of the material in g/cm^3.
+        specific_cost : float
+            Specific cost of the material in $/kg.
+        color : str
+            Color of the material.
+        """
+        self.density = density
+        self.specific_cost = specific_cost
+        self.name = name
+        self.color = color
+
+        self._last_updated = dt.now()
+        self._update_ranges()
+
+    def _update_ranges(self):
+        self._density_range = (self._density * 0.8, self._density * 1.2)
+        self._specific_cost_range = (self._specific_cost * 0.5, self._specific_cost * 3)
+
+    @property
+    def density(self):
+        return round(self._density * (KG_TO_G / M_TO_CM**3), 2)
+    
+    @property
+    def density_range(self):
+        return (
+            round(self._density_range[0] * (KG_TO_G / M_TO_CM**3), 2), 
+            round(self._density_range[1] * (KG_TO_G / M_TO_CM**3), 2)
+        )
+
+    @property
+    def density_marks(self):
+        min = np.ceil(self.density_range[0])
+        max = np.floor(self.density_range[1])
+        return {i: '' for i in range(int(min), int(max) + 1, 1)}
+
+    @property
+    def specific_cost(self):
+        return self._specific_cost
+    
+    @property
+    def specific_cost_range(self):
+        return (
+            round(self._specific_cost_range[0], 2), 
+            round(self._specific_cost_range[1], 2)
+        )
+
+    @property
+    def specific_cost_marks(self):
+        min = np.ceil(self.specific_cost_range[0])
+        max = np.floor(self.specific_cost_range[1])
+        return {i: '' for i in range(int(min), int(max) + 1, 5)}
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def color(self):
+        return self._color
+    
+    @property
+    def last_updated(self):
+        return self._last_updated.strftime("%Y-%m-%d %H:%M:%S")
+
+    @color.setter
+    def color(self, color: str) -> None:
+        self.validate_string(color, "Color")
+        self._color = color if color else "Unknown"
+
+    @density.setter
+    def density(self, density: float) -> None:
+        self.validate_positive_float(density, "Density")
+        self._density = density * G_TO_KG / CM_TO_M**3
+
+    @specific_cost.setter
+    def specific_cost(self, specific_cost: float) -> None:
+        self.validate_positive_float(specific_cost, "Specific Cost")
+        self._specific_cost = specific_cost
+
+    @name.setter
+    def name(self, name: str) -> None:
+        self.validate_string(name, "Name")
+        self._name = name
+
+    def __str__(self):
+        return f"{self.name}, {self.__class__.__name__}, {self.last_updated}"
+    
+    def __repr__(self):
+        return self.__str__()
+    
+
+
+class Metal(_Material):
+
+    def __init__(
+            self, 
+            name: str, 
+            density: float, 
+            specific_cost: float, 
+            color: str
+        ):
+
+        super().__init__(name, density, specific_cost, color)
+
+
+
+
+class Solvent(_Material):
+
+    def __init__(
+            self, 
+            name: str, 
+            density: float, 
+            specific_cost: float, 
+            color: str
+        ):
+
+        super().__init__(name, density, specific_cost, color)
+    
---
+++ steer_materials/CellMaterials/Base.py
@@ -0,0 +1,224 @@
+from steer_core.DataManager import DataManager
+from steer_core.Mixins.Serializer import SerializerMixin
+from steer_materials.Base import Metal, _Material
+
+
+class CurrentCollectorMaterial(Metal):
+    """
+    Materials from which current collectors are made.
+    """
+    def __init__(
+            self,
+            name: str,
+            density: float,
+            specific_cost: float,
+            color: str
+        ):
+        """
+        Current collector material for encapsulation of the cell
+        
+        Parameters
+        ----------
+        name : str
+            Name of the current collector material.
+        density : float
+            Density of the material in g/cm^3.
+        specific_cost : float
+            Specific cost of the material in $/kg.
+        color : str
+            Color of the material.
+        """
+        super().__init__(
+            name, 
+            density, 
+            specific_cost, 
+            color
+        )
+
+    @staticmethod
+    def from_database(name) -> 'CurrentCollectorMaterial':
+        """
+        Pull object from the database.
+        
+        Parameters
+        ----------
+        name : str
+            Name of the current collector material.
+
+        Returns
+        -------
+        CurrentCollectorMaterial: Instance of the class.
+
+        Raises
+        ------
+        ValueError: If the material is not found in the database.
+        """
+        database = DataManager()
+
+        available_materials = database.get_unique_values('current_collector_materials', 'name')
+
+        if name not in available_materials:
+            raise ValueError(f"Material '{name}' not found in the database. Available materials: {available_materials}")
+        
+        data = (
+            database
+            .get_current_collector_materials(most_recent=True)
+            .query(f"name == '{name}'")
+        )
+        
+        string_rep = data['object'].iloc[0]
+
+        material = SerializerMixin.deserialize(string_rep)
+
+        return material
+
+
+class InsulationMaterial(_Material):
+    """
+    Materials from which insulation is made.
+    """
+    def __init__(
+            self,
+            name: str,
+            density: float,
+            specific_cost: float,
+            color: str
+        ):
+        """
+        Insulation material for encapsulation of the cell
+        
+        Parameters
+        ----------
+        name : str
+            Name of the insulation material.
+        density : float
+            Density of the material in g/cm^3.
+        specific_cost : float
+            Specific cost of the material in $/kg.
+        color : str
+            Color of the material.
+        """
+        super().__init__(
+            name, 
+            density, 
+            specific_cost, 
+            color
+        )
+
+    @staticmethod
+    def from_database(name) -> 'InsulationMaterial':
+        """
+        Pull object from the database.
+
+        Parameters
+        ----------
+        name : str
+            Name of the insulation material.
+
+        Returns
+        -------
+        InsulationMaterial: Instance of the class.
+
+        Raises
+        ------
+        ValueError: If the material is not found in the database.
+        """
+        database = DataManager()
+        available_materials = database.get_unique_values('insulation_materials', 'name')
+
+        if name not in available_materials:
+            raise ValueError(f"Material '{name}' not found in the database. Available materials: {available_materials}")
+        
+        data = (
+            database
+            .get_data(table_name='insulation_materials')
+            .query(f"name == '{name}'")
+        )
+
+        string_data = data['object'].iloc[0]
+        material = SerializerMixin.deserialize(string_data)
+        return material
+
+    
+class SeparatorMaterial(_Material):
+    """
+    Materials from which separators are made.
+    """
+    def __init__(
+            self,
+            name: str,
+            density: float,
+            specific_cost: float,
+            porosity: float,
+            color: str
+        ):
+        """
+        Separator material for encapsulation of the cell
+
+        Parameters
+        ----------
+        name : str
+            Name of the separator material.
+        density : float
+            Density of the material in g/cm^3.
+        specific_cost : float
+            Specific cost of the material in $/kg.
+        porosity : float
+            Porosity of the separator material in %.
+        color : str
+            Color of the material.
+        """
+        super().__init__(
+            name,
+            density,
+            specific_cost,
+            color
+        )
+
+        self.porosity = porosity
+
+    @property
+    def porosity(self):
+        return round(self._porosity * 100, 2)
+    
+    @porosity.setter
+    def porosity(self, porosity: float) -> None:
+        self.validate_percentage(porosity, 'Porosity')
+        self._porosity = porosity / 100.0
+
+    @staticmethod
+    def from_database(name) -> 'SeparatorMaterial':
+        """
+        Pull object from the database.
+
+        Parameters
+        ----------
+        name : str
+            Name of the separator material.
+
+        Returns
+        -------
+        SeparatorMaterial: Instance of the class.
+
+        Raises
+        ------
+        ValueError: If the material is not found in the database.
+        """
+        database = DataManager()
+
+        available_materials = database.get_unique_values('separator_materials', 'name')
+
+        if name not in available_materials:
+            raise ValueError(f"Material '{name}' not found in the database. Available materials: {available_materials}")
+
+        data = (
+            database
+            .get_data(table_name='separator_materials')
+            .query(f"name == '{name}'")
+        )
+
+        string_data = data['object'].iloc[0]
+        material = SerializerMixin.deserialize(string_data)
+        return material
+
+
