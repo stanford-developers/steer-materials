7475dfc7b41620102fe081d670986450bfc6c6bd
Nicholas Siemons <nicholas9182@gmail.com>
2025-11-14 15:49:15 -0800

    updated_imports

--- steer_materials/__init__.py
+++ steer_materials/__init__.py
@@ -1,1 +1,6 @@
 __version__ = "0.1.14"
+
+# import base materials
+from .CellMaterials.Base import TapeMaterial, SeparatorMaterial, CurrentCollectorMaterial, InsulationMaterial
+from .CellMaterials.Electrode import AnodeMaterial, CathodeMaterial, ConductiveAdditive, Binder
+
