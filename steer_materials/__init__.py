1fb977415c5255e97676bf8450d6b858d44e27fd
Nicholas Siemons <nicholas9182@gmail.com>
2025-11-20 11:04:39 -0800

    small_changes

--- steer_materials/__init__.py
+++ steer_materials/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.14"
+__version__ = "0.1.16"
 
 # import base materials
 from .CellMaterials.Base import TapeMaterial, SeparatorMaterial, CurrentCollectorMaterial, InsulationMaterial
