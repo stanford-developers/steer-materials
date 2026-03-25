a3ae556780793d531b2dcca79bf3de1cff383a53
Nicholas Siemons <nicholas9182@gmail.com>
2026-03-25 13:16:42 -0700

    update_copyright_and_terms_conditions

--- steer_materials/Base.py
+++ steer_materials/Base.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024-2026 Nicholas Siemons
+# SPDX-FileCopyrightText: 2024-2026 Stanford University
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 from steer_core.Constants.Units import *
